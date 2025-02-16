2c2da3b76c977abc7824429ee22c7a1d03fd9a5c
KristijanPavlic <pavlictumpakristijan@gmail.com>
2025-02-16 21:58:28 +0100

    added bg lines to gallery section

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,6 +1,7 @@
 import { getTranslations } from '@/lib/getTranslations'
 import type { Translations } from '../types/translations'
 import { Frank_Ruhl_Libre } from 'next/font/google'
+import BackgroundLines from './ui/BackgroundLines'
 
 const frankRuhlLibre = Frank_Ruhl_Libre({
   weight: ['300', '400', '500', '600', '700', '800', '900'],
@@ -25,6 +26,7 @@ export default async function Gallery({
       id="gallery-section"
       className={`${frankRuhlLibre.className} container relative mx-auto -mt-20 h-[2000px] scroll-m-20`}
     >
+      <BackgroundLines />
       <h1>{translations.categories.weddings}</h1>
     </section>
   )
